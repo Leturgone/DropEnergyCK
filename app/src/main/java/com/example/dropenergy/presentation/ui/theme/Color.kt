a310a8cdc45eec1d8521b7d5a4fc98981bdcadf1
Leturgone <finekon221@gmail.com>
2025-04-27 12:07:10 +0300

    Сделана тема

---
+++ app/src/main/java/com/example/dropenergy/presentation/ui/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.example.dropenergy.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val Yellow40 = Color(0xFFFFC107)
+val Green = Color(0xFF4CAF50)
+
+
+
+
+val LightGreen  = Color(0xFF04D68D)
+val DarkBlue = Color(0xFF3E4A72)
+val LightYellow = Color(0xFFffbc8b)
+val LightRed = Color(0xFFffadc4)
+val LightGrey = Color(0xFFc5d1df)
+val White = Color(0xFFFEFEFE)
+val LightDarkBlue = Color(0xFF8894C8)
+
+val SoftBlue = Color(0xFFE4FBF4)
